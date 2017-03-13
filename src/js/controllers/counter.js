039cf4d0373dcd00ef6a8bda6b6e0490ff950dab
Marie Filbey <patriciamariefilbey@gmail.com>
2017-03-13 14:51:47 -0400

    Added Click handler to button.

--- src/js/controllers/counter.js
+++ src/js/controllers/counter.js
@@ -1,5 +1,9 @@
 function CounterController ($scope) {
   console.log("This is your Counter Controller.");
+
+  $scope.likeCount = function() {
+  console.log("cool");
+  };
 }
 
 export default CounterController;
