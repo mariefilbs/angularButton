9988003215cc7b2ff7e843b33f30faff47950638
Marie Filbey <patriciamariefilbey@gmail.com>
2017-03-13 16:42:52 -0400

    Added toggle button.

--- src/js/controllers/counter.js
+++ src/js/controllers/counter.js
@@ -2,15 +2,30 @@ function CounterController ($scope) {
   //console.log("This is your Counter Controller.");
   $scope.data = 0;
   $scope.name = 'likes';
+  var minus = true;
 
   $scope.likeCount = function() {
-    $scope.data++;
+    if (minus) {
+      $scope.data++;
+    } else {
+      $scope.data--;
+    }
     if ($scope.data === 1) {
       $scope.name = 'like';
     } else {
       $scope.name = 'likes';
     }
   };
+
+  $scope.likeDownCount = function () {
+    if (minus) {
+      minus = false;
+    } else {
+      minus = true;
+    }
+  }
+
+
 
 }
 
